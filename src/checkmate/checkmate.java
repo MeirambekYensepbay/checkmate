6b4936b033199b0132cad88d284cb268cd7cf412
Miko <32616763+MeirambekYensepbay@users.noreply.github.com>
2017-12-11 18:55:38 +0600

    update 11.12.17
    update 11.12.17

--- src/checkmate/checkmate.java
+++ src/checkmate/checkmate.java
@@ -7,8 +7,11 @@ import java.awt.event.MouseMotionListener;
 import java.util.Random;
 
 import javax.swing.*;
-	
 	public class checkmate extends JFrame{
+		boolean move_true=true;
+		boolean move_true1=true;
+		JLabel sts;
+		String status="Step";
 		int count = 2;
 		public int X=3*50+40;
 		public int Y=7*50+70;
@@ -32,128 +35,31 @@ import javax.swing.*;
 			ImageIcon wk = new ImageIcon("C:\\Users\\Yensepbay\\workspace\\checkmate\\src\\checkmate\\white_king.png");
 			ImageIcon wr1 = new ImageIcon("C:\\Users\\Yensepbay\\workspace\\checkmate\\src\\checkmate\\white_rook.png");
 			ImageIcon wr2 = new ImageIcon("C:\\Users\\Yensepbay\\workspace\\checkmate\\src\\checkmate\\white_rook.png");
+			
+			sts = new JLabel(status);
 			bkil = new JLabel(bk);
 			wkil = new JLabel(wk);
 			wril1 = new JLabel(wr1);
 			wril2 = new JLabel(wr2);
+			sts.setBounds(20, 10, 100, 50);
 			wkil.setBounds(X, Y, 50, 50);
 			wril1.setBounds(RX1, RY1, 50, 50);
 			wril2.setBounds(RX2, RY2, 50, 50);
 			setResizable(false);
 			setVisible(true);
 			bkil.setBounds(BX, BY, 50, 50);
+			
 			add(bkil);
 			add(wkil);
 			add(wril1);
 			add(wril2);
+			add(sts);
 			getContentPane().add(r);
 			
-			if(count%2==0){
-				wkil.addMouseListener(new MouseListener() {
-						
-						
-					@Override
-					public void mouseClicked(MouseEvent e) {
-						// TODO Auto-generated method stub
-							System.out.println("King choosed");
-							King_change_pos(X,Y);
-							count++;
-						}
-						
-						@Override
-						public void mouseEntered(MouseEvent e) {
-							// TODO Auto-generated method stub
-							
-						}
-
-						@Override
-						public void mouseExited(MouseEvent e) {
-							// TODO Auto-generated method stub
-							
-						}
-
-						@Override
-						public void mousePressed(MouseEvent e) {
-							// TODO Auto-generated method stub
-							
-						}
-
-						@Override
-						public void mouseReleased(MouseEvent e) {
-							// TODO Auto-generated method stub
-							
-						}
-					});
+				event();
+				
+			System.out.println(BX+" "+BY);
 			
-				wril1.addMouseListener(new MouseListener() {
-					
-					@Override
-					public void mouseReleased(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						System.out.println("Rook choosed");
-						rook_change_position(RX1, RY1);
-						
-					}
-					
-					@Override
-					public void mousePressed(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-					
-					@Override
-					public void mouseExited(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-					
-					@Override
-					public void mouseEntered(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-					
-					@Override
-					public void mouseClicked(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-				});
-				wril2.addMouseListener(new MouseListener() {
-					
-					@Override
-					public void mouseReleased(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						System.out.println("Rook choosed");
-						rook_change_position1(RX2, RY2);
-						
-					}
-					
-					@Override
-					public void mousePressed(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-					
-					@Override
-					public void mouseExited(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-					
-					@Override
-					public void mouseEntered(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-					
-					@Override
-					public void mouseClicked(MouseEvent arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-				});
-			}
 		}
 		
 		
@@ -164,13 +70,13 @@ import javax.swing.*;
 				@Override
 				public void mouseReleased(MouseEvent m) {
 					// TODO Auto-generated method stub
-					int movX=m.getX()-x;
-					int movY=m.getY()-y;
+				
+					int provx=(m.getX()-40)/50;
+					int provy=(m.getY()-70)/50;
 					
-					System.out.println(x+" "+y);
-					System.out.println(movX+" "+movY);
-					System.out.println(m.getX()+" "+m.getY());
-					move_king(x,y,movX,movY,m.getX(),m.getY());
+						move_king((X-40)/50,(Y-70)/50,provx,provy);
+					
+					
 				}
 				@Override
 				public void mousePressed(MouseEvent m) {
@@ -200,13 +106,11 @@ import javax.swing.*;
 		}
 	
 		public void rook_change_position(int x, int y){
-			this.RX1=x;
-			this.RY1=y;
 			r.addMouseListener(new MouseListener() {
 				
 				@Override
 				public void mouseReleased(MouseEvent m1) {
-					
+				
 				}
 				
 				@Override
@@ -230,17 +134,14 @@ import javax.swing.*;
 				@Override
 				public void mouseClicked(MouseEvent m1) {
 					// TODO Auto-generated method stub
-					// TODO Auto-generated method stub
-					int movX=m1.getX();
-					int movY=m1.getY();
+					// TODO Auto-generated method stub.
 					
-					System.out.println(x+" "+y);
-					System.out.println(movX+" "+movY);
-					System.out.println(m1.getX()+" "+m1.getY());
-					move_ladya(x,y,movX,movY);
+					int provx=(m1.getX()-40)/50;
+					int provy=(m1.getY()-70)/50;
+
+					move_ladya(provx,provy);
 				}
 			});
-			count++;
 		}
 		public void rook_change_position1(int x, int y){
 			this.RX2=x;
@@ -277,151 +178,55 @@ import javax.swing.*;
 					int movX=m1.getX();
 					int movY=m1.getY();
 					
-					move_ladya1(x,y,movX,movY);
+					move_ladya1(movX,movY);
 				}
 			});
-			count++;
 		}
 
-		public void move_ladya(int x, int y, int movX, int movY){
-			int cx=0;
-			int cy=0;
-			System.out.println(x+" "+y+" "+movX+" "+movY);
-			if(Math.abs(x-movX)<50&&movX>=40){
-				cx=movX-Math.abs(x-movX);
-				if(RY1>movY)
-					for(int i=RY1; i>=70; i-=50){
-						if(i>=movY&&movY+50>=i&&movY>=40){
-							cy=i-50;
-						}
-					}
-				else
-					for(int i=RY1; i<=520; i+=50){
-						if(i>movY&&movY+50>i&&movY<=520){
-							cy=i-50;
-						}
-					}
-				RX1=cx;
-				RY1=cy;
+		public void move_ladya(int movX, int movY){
+
+			if((RX1-40)/50==movX||(RY1-70)/50==movY){
+				RX1=movX*50+40;
+				RY1=movY*50+70;
+				wril1.setBounds(RX1, RY1, 50, 50);
+				repaint();
+				count++;
+				bot_step(BX,BY);
+				check_r(RX1,RY1);
 			}
-			else if(Math.abs(y-movY)<50&&movY>=40){
-				cy=movY-Math.abs(y-movY);
-				if(RX1>movX)
-					for(int i=RX1; i>=70; i-=50){
-						if(i>=movX&&movX+50>=i&&movX>=40){
-							cx=i-50;
-						}
-					}
-				else
-					for(int i=RX1; i<=520; i+=50){
-						if(i>movX&&movX+50>i&&movX<=520){
-							cx=i-50;
-						}
-					}
-				RX1=cx;
-				RY1=cy;
-			}
-			/*if(Math.abs(y-movX)<50){
-				cy=movY-Math.abs(y-movY);
-				RY1=cy;
-			}*/
-			wril1.setBounds(RX1, RY1, 50, 50);
-			System.out.println(RX1+" "+RY1);
-			repaint();
-			check_r(RX1,RY1);
+			else System.out.println("Error " + RX1 + " " + RY1);
 		}
-		public void move_ladya1(int x, int y, int movX, int movY){
-			int cx=0;
-			int cy=0;
-			System.out.println(x+" "+y+" "+movX+" "+movY);
-			if(Math.abs(x-movX)<50&&movX>=40){
-				cx=movX-Math.abs(x-movX);
-				if(RY2>movY)
-					for(int i=RY2; i>=70; i-=50){
-						if(i>=movY&&movY+50>=i&&movY>=40){
-							cy=i-50;
-						}
-					}
-				else
-					for(int i=RY2; i<=520; i+=50){
-						if(i>movY&&movY+50>i&&movY<=520){
-							cy=i-50;
-						}
-					}
-				RX2=cx;
-				RY2=cy;
+		public void move_ladya1(int movX, int movY){
+
+			if((RX2-40)/50==movX||(RY2-70)/50==movY){
+				RX2=movX*50+40;
+				RY2=movY*50+70;
+				wril2.setBounds(RX2, RY2, 50, 50);
+				repaint();
+				count++;
+				bot_step(BX,BY);
+				/*check_r(RX2,RY2);*/
 			}
-			else if(Math.abs(y-movY)<50&&movY>=40){
-				cy=movY-Math.abs(y-movY);
-				if(RX2>movX)
-					for(int i=RX2; i>=70; i-=50){
-						if(i>=movX&&movX+50>=i&&movX>=40){
-							cx=i-50;
-						}
-					}
-				else
-					for(int i=RX2; i<=520; i+=50){
-						if(i>movX&&movX+50>i&&movX<=520){
-							cx=i-50;
-						}
-					}
-				RX2=cx;
-				RY2=cy;
-			}
-			/*if(Math.abs(y-movX)<50){
-				cy=movY-Math.abs(y-movY);
-				RY1=cy;
-			}*/
-			wril2.setBounds(RX2, RY2, 50, 50);
-			System.out.println(RX2+" "+RY2);
-			repaint();
-			check_r(RX2,RY2);
+			else System.out.println("Error " + RX2 + " " + RY2);
 		}
 
-		public  void move_king(int x, int y, int movX, int movY, int fx,int fy){
-			int cx = 0;
-			int cy = 0;
-			if(Math.abs(X-fx)<=50&&Math.abs(X-fx)>=0&&Math.abs(Y-fy)<=50&&Math.abs(Y-fy)>=0){
-				
-				if(fx>X-50&&fx<=X&&fx>=0&&fx<=470){
-					System.out.println("1");
-					cx=fx-Math.abs(Math.abs(X-fx)-50);
-					X=cx;
-				}
-				if(fy>Y-50&&fy<=Y&&fy>=0&&fy<=470){
-					System.out.println("2");
-					cy=fy-Math.abs(Math.abs(Y-fy)-50);
-					Y=cy;
-				}
-				if(fy<Y+100&&fy>Y){
-					cy=fy-(Math.abs(Y-fy));
-					Y=cy;
-				}
-				if(fx<X+100&&fx>X){
-					cy=fy-(Math.abs(Y-fy));
-					Y=cy;
-				}
-				
-				
-			/*	for(int i=Y-50; i<=Y+50; i+=50){
-					for(int j=X-50; j<=X+50; j+=50){
-						if(fy<=i){
-							Y=i;
-						}
-						if(fx<=j){
-							X=j;
-						}
-							
-					}
-				}*/
-				}
-				else {System.out.println("Wrong");}
-				System.out.println(cx+" "+cy);
+		public void move_king(int x, int y, int movX, int movY){
+			if(((x-movX==0&&Math.abs(y-movY)==1)||((y-movY==0&&Math.abs(x-movX)==1))&&Math.abs((Math.abs(x-movX)-Math.abs(y-movY)))==1)||(Math.abs(x-movX)==1&&Math.abs(y-movY)==1)){
+				X=movX*50+40;
+				Y=movY*50+70;
 				wkil.setBounds(X, Y, 50, 50);
+				repaint();
+				count++;
+				bot_step(BX,BY);
 				check_r(X,Y);
-			repaint();
+				//return true;
+			}
+			else{
+				//return false;
+			}
 		}
-		public  void move_king_enemy(int x, int y, int movX, int movY, int fx,int fy){
+		
+		/*public  void move_king_enemy(int x, int y, int movX, int movY, int fx,int fy){
 			int cx = 0;
 			int cy = 0;
 				if(x==fx)
@@ -431,31 +236,45 @@ import javax.swing.*;
 				bkil.setBounds(BX, BY, 50, 50);
 			repaint();
 		}
+		*/
 		public  void move_king_enemy(int x, int y){
-			int cx = 0;
-			int cy = 0;
+			
 			Random r = new Random();
-			int a=r.nextInt()%10+10;
-				if(a>3&&a<6)
-					y=y+50;
-				else if(a<=3)
-					x=x+50;
-				else if(a>6&&a<9){
-					x=x+50;
-					y=y+50;
+			while(move_true){
+				double rand = Math.random()*8;
+				System.out.println("rand "+(int)rand+" x "+x);
+				if(((x-(int)rand==0)||(Math.abs(x-(int)rand)==1))){
+
+					System.out.println("rand "+(int)rand);
+					while(move_true1){
+						double rand1 = Math.random()*8;
+						System.out.println("rand1 "+(int)rand1+" y "+y);
+						/*if(((x-(int)rand==0)&&(Math.abs(y-(int)rand1)==1||(Math.abs(x-(int)rand)==1))&&(y-(int)rand1==0||(Math.abs(x-(int)rand)==1)&&Math.abs(y-(int)rand1)==1)&&Math.abs((Math.abs(x-(int)rand)-Math.abs(y-(int)rand1)))==1)){*/
+						if((x+1==(int)rand||x-1==(int)rand||x==(int)rand)&&(y+1==(int)rand1||y-1==(int)rand1)||y==(int)rand1){
+							
+							BX=(int)rand*50+40;
+							BY=(int)rand1*50+70;
+							bkil.setBounds(BX, BY, 50, 50);
+							repaint();
+							count++;
+							event();
+							move_true1=false;
+							
+						}
+					}
+					move_true=false;
 				}
-				BX=x;
-				BY=y;
-				bkil.setBounds(BX, BY, 50, 50);
-			repaint();
+			}
+				
 		}
 		public void check_r(int x, int y){
 			boolean b = false;
 			if(x==BX||y==BY){
 				System.out.println("Check");
 				b=true;
+				status="check";
 			}
-			if(b)
+			if(b==true)
 				bot_step(x,y);
 			
 		} 
@@ -464,15 +283,125 @@ import javax.swing.*;
 			new checkmate();
 		}
 		
-		public void bot_step(int q, int w){
-			Random r = new Random();
-				int rnd1 = 40 + (int)(Math.random() * ((510 - 40) + 1));
-				int rnd2 = 70 + (int)(Math.random() * ((540 - 70) + 1));
-				move_king_enemy(BX,BY,50,50,q,w);
-				
+	
+		public void bot_step(int x, int y){
+			status="black";
+			repaint();
+			move_true=true;
+			move_true1=true;
+			move_king_enemy((BX-40)/50,(BY-70)/50);
+			System.out.println(BX+" "+BY);
 		}
-		public void bot_step(){
-			move_king_enemy(BX,BY);
+		public void event(){
+				status="White";
+				repaint();
+				wkil.addMouseListener(new MouseListener() {
+				
+				
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// TODO Auto-generated method stub
+						
+					System.out.println("King choosed");
+					King_change_pos(X,Y);
+					
+					}
+					
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mouseExited(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mousePressed(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mouseReleased(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
+		
+				wril1.addMouseListener(new MouseListener() {
+				
+				@Override
+				public void mouseReleased(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					
+					
+				}
+				
+				@Override
+				public void mousePressed(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseExited(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseEntered(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					status="Root choosed";
+					System.out.println("Rook choosed");
+					rook_change_position(RX1, RY1);
+				}
+			});
+				wril2.addMouseListener(new MouseListener() {
+				
+				@Override
+				public void mouseReleased(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+				
+					
+				}
+				
+				@Override
+				public void mousePressed(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseExited(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseEntered(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					System.out.println("Rook choosed");
+					rook_change_position1(RX2, RY2);
+				}
+			});
+			
 		}
 	}
 	
@@ -491,5 +420,3 @@ import javax.swing.*;
 				}
 			}
 	}
-	
-	
